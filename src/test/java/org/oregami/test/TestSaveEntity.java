8cbfb982e11f9d9c9889b2d2edaba5b50aa8e934
gene-oregami <sebastian@kultpower.de>
2013-01-16 22:43:59 +0100

    tests for saving and loading entities

--- src/test/java/org/oregami/test/TestSaveEntity.java
+++ src/test/java/org/oregami/test/TestSaveEntity.java
@@ -1,8 +1,12 @@
 package org.oregami.test;
 
+import java.util.Iterator;
+
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.oregami.entities.FirstEntity;
 import org.oregami.jpa.IFirstEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -25,6 +29,24 @@ public class TestSaveEntity {
 	
 	@Test
 	public void testSaveEntity() {
-		System.out.println("firstEntityRepository: " + firstEntityRepository);
+		Assert.assertNotNull(firstEntityRepository);
+		
+		FirstEntity entity = new FirstEntity();
+		entity.setStringField("one");
+		firstEntityRepository.save(entity);
+
+		FirstEntity entity2 = new FirstEntity();
+		entity2.setStringField("two");
+		firstEntityRepository.save(entity2);
+		
+		
+		Iterator<FirstEntity> iterator = firstEntityRepository.findAll().iterator();
+		while (iterator.hasNext()) {
+			FirstEntity e = (FirstEntity) iterator.next();
+			System.out.println("found entity: " + e.getStringField());
+		}
+		
+		Assert.assertEquals(2L, firstEntityRepository.count());
+		
 	}
 }
